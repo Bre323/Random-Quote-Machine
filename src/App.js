81a54c6ff78846a12a30cfac108c03305dbf0a72
Breno Rocha <brenopenarocha2@gmail.com>
2023-03-03 20:18:42 -0300

    fetching API

--- src/App.js
+++ src/App.js
@@ -1,6 +1,31 @@
 import React from "react";
 
+//quotes API
+const API = 'https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json'
+
+
 class App extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      quotes: [],
+      index: 0
+    }
+  }
+
+
+  componentDidMount() {
+    fetch(API)
+    .then(res => res.json())
+    .then(data => {
+      this.setState({
+        quotes: data.quotes[0]
+      })
+    })
+  }
+
+  
   render() {
     return (
       <div className="d-flex justify-content-center align-items-center vh-100">
